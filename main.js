4f78a8d6568b24ef8d8b84c7942f115fada926ec
Mahmoud <you@example.com>
2025-12-27 20:40:44 +0200

    add start game page

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-document.querySelectorAll(".card").forEach(card => {
-    card.addEventListener("click", () => {
-      card.classList.toggle("is-flipped");
-    });
-  });
-  
+document.querySelector(".control-buttons span").addEventListener('click',()=>{
+
+  let yourName = prompt("What's your name?")
+
+  if(yourName == null || yourName==""){
+    document.querySelector(".name span").innerHTML ="Unknown"
+  }else{
+    document.querySelector(".name span").innerHTML = yourName;
+  }
+  document.querySelector(".control-buttons").remove()
+})
+
+let duration = 1000;
+
+let blockContainer = document.querySelector(".memory-game-blocks");
+
+let blocks = Array.from(blockContainer.children)
+
+let orderRange = [...Array(blocks.length).keys()]
+
